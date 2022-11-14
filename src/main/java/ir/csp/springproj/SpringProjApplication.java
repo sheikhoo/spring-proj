4164e6cc1ae58e9a51e3c49e206247a9655d09d7
sheikh <sheikh.csp@gmail.com>
2022-11-14 09:39:48 +0330

    change

--- src/main/java/ir/csp/springproj/SpringProjApplication.java
+++ src/main/java/ir/csp/springproj/SpringProjApplication.java
@@ -2,10 +2,17 @@ package ir.csp.springproj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringProjApplication {
+public class SpringProjApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringProjApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringProjApplication.class, args);
 	}
